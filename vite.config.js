6db7ae4356511000ae6e69f6c47565afdff9eb56
Exishe <manukyaneghishe@gmail.com>
2023-09-05 19:17:42 +0400

    configure vite

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/threejsportfolio2/',
+  base: 'https://Egman123.github.io/myportfolio/',
   plugins: [react()],
 })
